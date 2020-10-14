cb7555b7378342984caf7d4eac9f0bb2e5b71a89
adambirse <adambirse@gmail.com>
2020-10-14 15:41:42 +0100

    correct typo.

--- packages/stack/bin/office-api.ts
+++ packages/stack/bin/office-api.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { ReactshoppeApiStack } from '../reactshoppe-api-stack';
+import 'source-map-support/register';
+import { OfficeApiStack } from '../office-api-stack';
 
 const app = new cdk.App();
-new ReactshoppeApiStack(app, 'ReactshoppeApiStack', { description: 'Simple sample ecommerce service with New Relic' });
+new OfficeApiStack(app, 'OfficeApiStack', { description: 'Office' });
