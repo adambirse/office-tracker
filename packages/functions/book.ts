40c8270cd83424d77d967e7c96dae705dbc3413e
adambirse <adambirse@gmail.com>
2020-10-14 12:38:47 +0100

    optimise db client

--- packages/functions/book.ts
+++ packages/functions/book.ts
@@ -4,13 +4,13 @@ import { v4 as uuidv4 } from 'uuid';
 import { TableNames } from 'office-database/TableNames';
 import { success, failure } from './response';
 
+const ddb = new AWS.DynamoDB.DocumentClient();
+
 export const book = async (event: APIGatewayProxyEvent) => {
-  const ddb = new AWS.DynamoDB.DocumentClient();
   let newOrder = {};
   // NOTE: in a real application, we’d do more to validate input
   if (event.body != null) {
     const { date, user } = JSON.parse(event.body);
-    const ddb = new AWS.DynamoDB.DocumentClient();
     const newBooking = {
       id: uuidv4(),
       date,
